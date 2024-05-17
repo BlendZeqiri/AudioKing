24192176394ab46c1f85434cf373be32e6ceb212
BlendZeqiri <bzrmarketingagency@gmail.com>
2024-05-17 21:24:05 +0200

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -1,6 +1,7 @@
-﻿// MainForm.cs
 using System;
 using System.Windows.Forms;
+using System.Media;
+using System.IO;
 
 namespace SoundKing
 {
@@ -9,22 +10,33 @@ namespace SoundKing
         private Timer timer;
         private string selectedMusicFilePath;
         private int timeLeft;
+        private SoundPlayer player;
 
         public MainForm()
         {
-            object value = InitializeComponent();
+            InitializeComponent();
             timer = new Timer();
             timer.Interval = 1000; // 1 second intervals
             timer.Tick += Timer_Tick;
         }
 
+        private void btnSelectMusic_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Audio Files|*.wav;*.mp3";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedMusicFilePath = openFileDialog.FileName;
+                lblSelectedFile.Text = Path.GetFileName(selectedMusicFilePath);
+            }
+        }
 
         private void btnStartTimer_Click(object sender, EventArgs e)
         {
             if (int.TryParse(txtTimer.Text, out timeLeft) && !string.IsNullOrEmpty(selectedMusicFilePath))
             {
                 timer.Start();
-                ShowStopForm(); // Show StopForm when music starts
             }
             else
             {
@@ -47,14 +59,42 @@ namespace SoundKing
 
         private void PlayMusic()
         {
-            // Play music logic
-            // Show StopForm here as well
+            try
+            {
+                if (Path.GetExtension(selectedMusicFilePath).Equals(".wav", StringComparison.OrdinalIgnoreCase))
+                {
+                    player = new SoundPlayer(selectedMusicFilePath);
+                    player.Play();
+                    ShowStopForm();
+                }
+                else
+                {
+                    MessageBox.Show("MP3 playback requires additional libraries. Only WAV files are supported out of the box.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error playing music: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void ShowStopForm()
         {
-            StopForm stopForm = new StopForm();
+            StopForm stopForm = new StopForm(player);
             stopForm.ShowDialog();
+        }
+
+        private void txtTimer_TextChanged(object sender, EventArgs e)
+        {
+            // Event handler logic for the txtTimer_TextChanged event
+            // This method can be left empty or populated with the desired functionality
+        }
+
+        private void lblTimer_Click(object sender, EventArgs e)
+        {
+            // Event handler logic for the lblTimer_Click event
+            // This method can be left empty or populated with the desired functionality
         }
     }
 }
+
